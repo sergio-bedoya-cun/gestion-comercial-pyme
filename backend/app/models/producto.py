6d036297b3fd1f417efb053ce1e09615a367ab12
sergio-bedoya-cun <sergio.bedoya@cun.edu.co>
2026-05-13 16:04:51 -0500

    Added boolean

--- backend/app/models/producto.py
+++ backend/app/models/producto.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, Text, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
